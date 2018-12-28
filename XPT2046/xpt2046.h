7e092d03be62dd45610f6f288a7532ea4503b0bd
Anobium <evan@anobium.co.uk>
2018-12-28 18:27:11 +0000

    Updated the library online. Many updates....

--- XPT2046/xpt2046.h
+++ XPT2046/xpt2046.h
@@ -15,14 +15,38 @@
 '    License along with this library; if not, write to the Free Software
 '    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+'    v0.9b
+
+ '''    '******************************************************************************************************
+'''    'Setup the XPT2046
+'''
+'''    'Typical calibration range for Hardware SPI when using XPT2046
+'''    'for 320 * 240 screeen... may be different for others
+'''    #define XPT2046XMIN   17
+'''    #define XPT2046YMIN   15
+'''    #define XPT2046XMAX   100
+'''    #define XPT2046YMAX   110
+'''
+'''    'Typical Calibration range for Software SPI when using XPT2046
+''''    #define XPT2046XMIN   30
+''''    #define XPT2046YMIN   25
+''''    #define XPT2046XMAX   210
+''''    #define XPT2046YMAX   225
+'''
+'''
+'''    #define XPT2046_DI       DIGITAL_12          ' Data in | MISO.  Should be shared port the GLCD (it will be if hardware SPI is used).  No essential to define as the librart will select the GLCD port
+'''    #define XPT2046_DO       DIGITAL_11          ' Data out | MOSI.  .  Should be shared port the GLCD (it will be if hardware SPI is used).  No essential to define as the librart will select the GLCD port
+'''    #define XPT2046_SCK      DIGITAL_13          ' Clock Line.  .  Should be shared port the GLCD (it will be if hardware SPI is used).  No essential to define as the librart will select the GLCD port
+'''    #define XPT2046_CS       DIGITAL_2           ' Touch chip select line. MUST BE DEFINED.
+'''    #define XPT2046_IRQ      PORTc.5             ' Event line... something touched the GLCD touch sensor.  MUST BE DEFINED.
+'''    #define XPT2046_HardwareSPI                  ' remove/comment out if you want to use software SPI - must use same method as GLCD
+'''
+'''    '******************************************************************************************************
+
+  
+  
+'No change below here  
 #define XPT2046_ReadSamples 15
-
-'Define Precision
-#define PREC_LOW        1
-#define PREC_MEDIUM     2
-#define PREC_HI         3
-#define PREC_EXTREME    4
-
 
 #define XPT2046_CFG_START   128       'bit 7
 
@@ -43,11 +67,6 @@
 
 #define XPT2046_CFG_PWR     0         'bit 1-0 - the power mask.  PD1 & PD0
 
-
-
-
-'Pin mappings for XPT2046
-'#define XPT2046_CS  portn.n
 
 #script
 
@@ -94,15 +113,24 @@
       Warning "Missing Port Assignment XPT2046_SCK"
   end if
 
+  XPT2046_CS_PORT = 0
+  if XPT2046_CS Then
+    XPT2046_CS_PORT = 1
+  end if
+  if XPT2046_CS_PORT = 0 Then
+      Warning "Missing Port Assignment XPT2046_CS"
+  end if
 
-
+  XPT2046_IRQ_PORT = 0
+  if XPT2046_IRQ Then
+    XPT2046_IRQ_PORT = 1
+  end if
+  if XPT2046_IRQ_PORT = 0 Then
+      Warning "Missing Port Assignment XPT2046_IRQ"
+  end if
 
 
 #endscript
-
-'#define XPT2046_DI  GLCD_DI
-'#define XPT2046_DO  GLCD_DO
-'#define XPT2046_SCK GLCD_SCK
 
 #startup Init_XPT2046, 99
 
@@ -153,35 +181,6 @@ Sub EnableIRQ_XPT2046
 
 End Sub
 
-
-'''Send a command to the XPT2046 GLCD
-'''@param XPT2046SendByte Command to send
-'''@hide
-sub  SendData_XPT2046( IN XPT2046SendByte as byte )
-
-
-  #ifdef XPT2046_HardwareSPI
-     SPITransfer  XPT2046SendByte,  XPT2046TempOut
-  #endif
-
-  #ifndef XPT2046_HardwareSPI
-      set XPT2046_SCK Off;
-      repeat 8
-
-        if XPT2046SendByte.7 = ON  then
-          set XPT2046_DO ON;
-        else
-          set XPT2046_DO OFF;
-        end if
-        SET XPT2046_SCK On;
-        rotate XPT2046SendByte left
-        set XPT2046_SCK Off;
-
-      end repeat
-  #endif
-
-end Sub
-
 '''Send a data byte to the XPT2046 GLCD
 '''@param XPT2046SendByte Byte to send
 '''@hide
@@ -219,87 +218,11 @@ sub  TransferData_XPT2046( IN XPT2046SendByte as byte, XPT2046OutByte as byte )
 
 end Sub
 
-'''Send a data word (16 bits) to the XPT2046 GLCD
-'''@param XPT2046SendByte Word to send
-'''@hide
-Sub SendWord_XPT2046(In XPT2046SendWord As Word)
-
-  dim XPT2046SendWord as word
-
-
-  #ifdef XPT2046_HardwareSPI
-     SPITransfer  XPT2046SendWord_H,  XPT2046TempOut
-     SPITransfer  XPT2046SendWord,  XPT2046TempOut
-
-     exit sub
-  #endif
-
-  #ifndef XPT2046_HardwareSPI
-  repeat 16
-
-    if XPT2046SendWord.15 = ON then
-      set XPT2046_DO ON;
-    else
-      set XPT2046_DO OFF;
-    end if
-    SET XPT2046_SCK On;
-    rotate XPT2046SendWord left
-    set XPT2046_SCK Off;
-
-  end repeat
-
-  #endif
-
-End Sub
-
-
-Function Read_XPT2046  as  Word
-
-
-  #ifdef XPT2046_HardwareSPI
-     SPITransfer  XPT2046SendByte,  Read_XPT2046_h
-     SPITransfer  XPT2046SendByte,  Read_XPT2046
-
-     exit sub
-  #endif
-
-  #ifndef XPT2046_HardwareSPI
-    repeat 16
-
-      if XPT2046_DO = ON then
-        Read_XPT2046.15 = 1
-      else
-        Read_XPT2046.15 = 0
-      end if
-      SET XPT2046_SCK On;
-      rotate Read_XPT2046 left
-      set XPT2046_SCK Off;
-
-    end Repeat
-
-  #endif
-
-End Function
-
 Sub GetXY_XPT2046 (out XTouchPoint_XPT2046 as word, out YTouchPoint_XPT2046 as word )
 
     dim XTouchPoint_XPT2046, YTouchPoint_XPT2046 as Word
 
-    'Send 0x90
-    'Read
-    'Send 0xD0
-    'Read
     set XPT2046_CS OFF
-'        TransferData_XPT2046( XPT2046_CFG_START | XPT2046_CFG_12BIT | XPT2046_CFG_DFR | XPT2046_MUX_Y  | XPT2046_CFG_PWR, YTouchPoint_XPT2046 )
-'
-'        TransferData_XPT2046( XPT2046_CFG_START | XPT2046_CFG_12BIT | XPT2046_CFG_DFR | XPT2046_MUX_X  | XPT2046_CFG_PWR, XTouchPoint_XPT2046 )
-'
-'        TransferData_XPT2046( XPT2046_CFG_START | XPT2046_CFG_12BIT | XPT2046_CFG_DFR | XPT2046_MUX_Z1 | XPT2046_CFG_PWR, Z1TouchPoint_XPT2046 )
-'
-'        TransferData_XPT2046( XPT2046_CFG_START | XPT2046_CFG_12BIT | XPT2046_CFG_DFR | XPT2046_MUX_Z2 | XPT2046_CFG_PWR, Z2TouchPoint_XPT2046 )
-'TransferData_XPT2046( 0x98, YTouchPoint_XPT2046 )
-'TransferData_XPT2046( 0xD8, YTouchPoint_XPT2046 )
-'TransferData_XPT2046( 0xD8, XTouchPoint_XPT2046 )
 
     Repeat 2
         TransferData_XPT2046  ( XPT2046_CFG_START | XPT2046_CFG_8BIT | XPT2046_CFG_DFR | XPT2046_MUX_X  | XPT2046_CFG_PWR ), XPT2046TempOut
@@ -312,12 +235,11 @@ Sub GetXY_XPT2046 (out XTouchPoint_XPT2046 as word, out YTouchPoint_XPT2046 as w
     dim NewXTouchPoint_XPT2046, NewYTouchPoint_XPT2046 as Word
     dim XTouchPoint_XPT2046raw, YTouchPoint_XPT2046Raw as word
 
-
     Repeat XPT2046_ReadSamples
 
-        TransferData_XPT2046  0x98, XPT2046TempOut
+        TransferData_XPT2046  XPT2046_CFG_START | XPT2046_CFG_8BIT | XPT2046_CFG_DFR | XPT2046_MUX_X  | XPT2046_CFG_PWR, XPT2046TempOut
         TransferData_XPT2046  0,  NewYTouchPoint_XPT2046
-        TransferData_XPT2046  0xD8, XPT2046TempOut
+        TransferData_XPT2046  XPT2046_CFG_START | XPT2046_CFG_8BIT | XPT2046_CFG_DFR | XPT2046_MUX_Y  | XPT2046_CFG_PWR, XPT2046TempOut
         TransferData_XPT2046  0, NewXTouchPoint_XPT2046
         TransferData_XPT2046  0, XPT2046TempOut
 
@@ -325,37 +247,20 @@ Sub GetXY_XPT2046 (out XTouchPoint_XPT2046 as word, out YTouchPoint_XPT2046 as w
         XTouchPoint_XPT2046 = ( XTouchPoint_XPT2046 + NewXTouchPoint_XPT2046 )
 
     End Repeat
-      yTouchPoint_XPT2046 = yTouchPoint_XPT2046 /(XPT2046_ReadSamples + 1)
-      xTouchPoint_XPT2046 = xTouchPoint_XPT2046 /(XPT2046_ReadSamples + 1)
-
+    yTouchPoint_XPT2046 = yTouchPoint_XPT2046 /(XPT2046_ReadSamples + 1)
+    xTouchPoint_XPT2046 = xTouchPoint_XPT2046 /(XPT2046_ReadSamples + 1)
+      
     set XPT2046_CS ON
 
     XTouchPoint_XPT2046raw = XTouchPoint_XPT2046
     YTouchPoint_XPT2046Raw = yTouchPoint_XPT2046
 
-
     XTouchPoint_XPT2046 = scale ( XTouchPoint_XPT2046, XPT2046_Xmin, XPT2046_Xmax, 0, Current_GLCD_WIDTH )
     YTouchPoint_XPT2046 = scale ( YTouchPoint_XPT2046, XPT2046_Ymin, XPT2046_Ymax, 0, Current_GLCD_HEIGHT )
-
-
 
     if GLCDRotateState = Portrait_Rev then
         YTouchPoint_XPT2046 = GLCD_HEIGHT - YTouchPoint_XPT2046
     end if
 
-
-
-
 End Sub
 
-
-
-Sub SetPrecision_XPT2046(In precision as Byte)
-  Select Case precision
-         Case PREC_LOW: prec=1
-         Case PREC_MEDIUM: prec=12
-         Case PREC_HI: prec=27
-         Case PREC_EXTREME: prec=102
-         Case else : prec=12
-  End Select
-End Sub
